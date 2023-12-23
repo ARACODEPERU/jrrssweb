afadb6a846656824981c06be109d3b19e48400ab
Grandejosh <60234854+Grandejosh@users.noreply.github.com>
2023-12-23 03:42:15 -0500

    DarkMODE

--- resources/views/components/jrrss/header-area.blade.php
+++ resources/views/components/jrrss/header-area.blade.php
@@ -1,5 +1,109 @@
 <div>
-    <header id="header" data-plugin-options="{'stickyEnabled': true, 'stickyEnableOnBoxed': true, 'stickyEnableOnMobile': false, 'stickyStartAt': 164, 'stickySetTop': '-164px', 'stickyChangeLogo': false}">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <style>
+        /* Estilos para Switch */
+        .switch {
+            width: 60px;
+            height: 25px;
+            padding: 0 5px;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            position: relative;
+            border-radius: 100px;
+            border: #333 solid 2px;
+            background-color: #132b41;
+        }
+
+        .switch i {
+            font-size: 1rem;
+            color: #fdc21c;
+        }
+
+        /*Accede al ultimo item de la lista */
+        ul.nav li.switch::before {
+            content: "";
+            position: absolute;
+            height: 25px;
+            width: 25px;
+            border-radius: 50%;
+            background-color: #fff;
+            border: solid thin #000;
+            left: 0;
+            right: unset;
+        }
+
+        ul.nav li.switch.active::before {
+            right: 0;
+            left: unset;
+            border: solid thin #fff;
+        }
+
+        li.switch.active i {
+            color: #fff
+        }
+
+        body.dark .header-container {
+            background-color: #313131;
+        }
+
+        body.dark .main {
+            background-color: #313131;
+        }
+
+        body.dark div.body {
+            background-color: #313131;
+        }
+
+        body.dark nav {
+            background-color: #313131;
+        }
+
+        body.dark nav ul {
+            background-color: #313131;
+        }
+
+        a.dropdown-item.dark {
+            color: white;
+        }
+    </style>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+          const sswitch = document.querySelector(".switch");
+      
+          if (localStorage.getItem('darkMode') === 'true') {
+            sswitch.classList.add("active");
+            document.body.classList.add("dark");
+            const a_tags = document.querySelectorAll(".dropdown-item");
+            a_tags.forEach((a_tag) => {
+              a_tag.classList.add("dark");
+            });
+          }
+      
+          sswitch.addEventListener("click", e => {
+            darkMode(sswitch);
+          });
+      
+          function darkMode(sswitch) {
+            let darkMode = localStorage.getItem('darkMode');
+      
+            if (darkMode === 'true') {
+              localStorage.setItem('darkMode', 'false');
+            } else {
+              localStorage.setItem('darkMode', 'true');
+            }
+      
+            sswitch.classList.toggle("active");
+            document.body.classList.toggle("dark");
+            const a_tags = document.querySelectorAll(".dropdown-item");
+            a_tags.forEach((a_tag) => {
+              a_tag.classList.toggle("dark");
+            });
+          }
+        });
+      </script>
+    <header id="header"
+        data-plugin-options="{'stickyEnabled': true, 'stickyEnableOnBoxed': true, 'stickyEnableOnMobile': false, 'stickyStartAt': 164, 'stickySetTop': '-164px', 'stickyChangeLogo': false}">
         <div class="header-body border-0">
             <div class="header-top header-top-default border-bottom-0 bg-color-dark">
                 <div class="container">
@@ -9,13 +113,20 @@
                                 <nav class="header-nav-top">
                                     <ul class="nav nav-pills text-uppercase text-2">
                                         <li class="nav-item nav-item-anim-icon" style="padding: 0px 10px;">
-                                            <a class="nav-link pe-0 text-light opacity-7" href=""><i class="far fa-play-circle"></i> EN VIVO</a>
+                                            <a class="nav-link pe-0 text-light opacity-7" href=""><i
+                                                    class="far fa-play-circle"></i> EN VIVO</a>
                                         </li>
                                         <li class="nav-item nav-item-anim-icon" style="padding: 0px 10px;">
-                                            <a class="nav-link pe-0 text-light opacity-7" href=""><i class="fas fa-university"></i> CAMPUS VIRTUAL</a>
+                                            <a class="nav-link pe-0 text-light opacity-7" href=""><i
+                                                    class="fas fa-university"></i> CAMPUS VIRTUAL</a>
                                         </li>
                                         <li class="nav-item nav-item-anim-icon" style="padding: 0px 10px;">
-                                            <a class="nav-link pe-0 text-light opacity-7" href=""><i class="fa fa-key"></i> Intranet</a>
+                                            <a class="nav-link pe-0 text-light opacity-7" href=""><i
+                                                    class="fa fa-key"></i> Intranet</a>
+                                        </li>
+                                        <li class="switch">
+                                            <i class="bx bxs-sun"></i>
+                                            <i class="bx bxs-moon"></i>
                                         </li>
                                     </ul>
                                 </nav>
@@ -23,11 +134,16 @@
                         </div>
                         <div class="header-column justify-content-end">
                             <div class="header-row">
-                                <ul class="header-social-icons social-icons d-none d-sm-block social-icons-clean social-icons-icon-light">
-                                    <li class="social-icons-facebook"><a href="http://www.facebook.com/" target="_blank" title="Facebook"><i class="fab fa-facebook-f"></i></a></li>
-                                    <li class="social-icons-instagram"><a href="http://www.instagram.com/" target="_blank" title="Instagram"><i class="fab fa-instagram"></i></a></li>
-                                    <li class="social-icons-youtube"><a href="http://www.youtube.com/" target="_blank" title="youtube"><i class="fab fa-youtube"></i></a></li>
-                                    <li class="social-icons-twitter"><a href="http://www.twitter.com/" target="_blank" title="Twitter"><i class="fab fa-tiktok"></i></a></li>
+                                <ul
+                                    class="header-social-icons social-icons d-none d-sm-block social-icons-clean social-icons-icon-light">
+                                    <li class="social-icons-facebook"><a href="http://www.facebook.com/" target="_blank"
+                                            title="Facebook"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li class="social-icons-instagram"><a href="http://www.instagram.com/"
+                                            target="_blank" title="Instagram"><i class="fab fa-instagram"></i></a></li>
+                                    <li class="social-icons-youtube"><a href="http://www.youtube.com/" target="_blank"
+                                            title="youtube"><i class="fab fa-youtube"></i></a></li>
+                                    <li class="social-icons-twitter"><a href="http://www.twitter.com/" target="_blank"
+                                            title="Twitter"><i class="fab fa-tiktok"></i></a></li>
                                 </ul>
                             </div>
                         </div>
@@ -36,7 +152,8 @@
             </div>
             <div class="ara_centrado_total">
                 <img style="max-width: 100%;
-                height: auto;" src="{{ asset('themes/jrrss/assets/img/cabecera-WEB.jpg') }}" alt="">
+                height: auto;"
+                    src="{{ asset('themes/jrrss/assets/img/cabecera-WEB.jpg') }}" alt="">
             </div>
             <div class="header-nav-bar header-nav-bar-top-border bg-light">
                 <div class="header-container ">
@@ -44,74 +161,89 @@
                         <div class="header-column">
                             <div class="header-row justify-content-end">
                                 <div class="header-nav p-0">
-                                    <div class="header-nav header-nav-line header-nav-divisor header-nav-spaced justify-content-lg-center">
-                                        <div class="header-nav-main header-nav-main-square header-nav-main-effect-1 header-nav-main-sub-effect-1">
+                                    <div
+                                        class="header-nav header-nav-line header-nav-divisor header-nav-spaced justify-content-lg-center">
+                                        <div
+                                            class="header-nav-main header-nav-main-square header-nav-main-effect-1 header-nav-main-sub-effect-1">
                                             <nav class="collapse">
                                                 <ul class="nav nav-pills flex-column flex-lg-row" id="mainNav">
                                                     <li class="dropdown">
-                                                        <a class="dropdown-item {{ request()->routeIs('cms_principal') ? 'active' : '' }}" href="{{ route('cms_principal') }}">
+                                                        <a class="dropdown-item {{ request()->routeIs('cms_principal') ? 'active' : '' }}"
+                                                            href="{{ route('cms_principal') }}">
                                                             HOME
                                                         </a>
                                                     </li>
                                                     <li class="dropdown">
-                                                        <a class="dropdown-item {{ request()->routeIs('web_quienes_somos') ? 'active' : '' }}" href="{{ route('web_quienes_somos') }}">
+                                                        <a class="dropdown-item {{ request()->routeIs('web_quienes_somos') ? 'active' : '' }}"
+                                                            href="{{ route('web_quienes_somos') }}">
                                                             ¿QUIENES SOMOS?
                                                         </a>
                                                     </li>
                                                     <li class="dropdown">
-                                                        <a class="dropdown-item {{ request()->routeIs('web_sedes') ? 'active' : '' }}" href="{{ route('web_sedes') }}">
+                                                        <a class="dropdown-item {{ request()->routeIs('web_sedes') ? 'active' : '' }}"
+                                                            href="{{ route('web_sedes') }}">
                                                             SEDES
                                                         </a>
                                                     </li>
                                                     <li class="dropdown">
-                                                        <a class="dropdown-item {{ request()->routeIs('web_cobertura') ? 'active' : '' }}" href="{{ route('web_cobertura') }}">
+                                                        <a class="dropdown-item {{ request()->routeIs('web_cobertura') ? 'active' : '' }}"
+                                                            href="{{ route('web_cobertura') }}">
                                                             COBERTURA
                                                         </a>
                                                     </li>
                                                     <li class="dropdown">
-                                                        <a class="dropdown-item {{ request()->routeIs('web_eventos') ? 'active' : '' }}" href="{{ route('web_eventos') }}">
+                                                        <a class="dropdown-item {{ request()->routeIs('web_eventos') ? 'active' : '' }}"
+                                                            href="{{ route('web_eventos') }}">
                                                             EVENTOS
                                                         </a>
                                                     </li>
                                                     <li class="dropdown">
-                                                        <a class="dropdown-item {{ request()->routeIs('escueloa') ? 'active' : '' }}" href="">
+                                                        <a class="dropdown-item {{ request()->routeIs('escueloa') ? 'active' : '' }}"
+                                                            href="">
                                                             ESCUELA SOBRENATURAL
                                                         </a>
                                                     </li>
                                                     <li class="dropdown">
-                                                        <a class="dropdown-item {{ request()->routeIs('escueloa') ? 'active' : '' }}" href="">
+                                                        <a class="dropdown-item {{ request()->routeIs('escueloa') ? 'active' : '' }}"
+                                                            href="">
                                                             ECELT
                                                         </a>
                                                     </li>
                                                     <li class="dropdown">
-                                                        <a class="dropdown-item {{ request()->routeIs('web_rmnt') ? 'active' : '' }}" href="{{ route('web_rmnt') }}">
+                                                        <a class="dropdown-item {{ request()->routeIs('web_rmnt') ? 'active' : '' }}"
+                                                            href="{{ route('web_rmnt') }}">
                                                             RMNT
                                                         </a>
                                                     </li>
                                                     <li class="dropdown">
-                                                        <a class="dropdown-item {{ request()->routeIs('web_kids') ? 'active' : '' }}" href="{{ route('web_kids') }}">
+                                                        <a class="dropdown-item {{ request()->routeIs('web_kids') ? 'active' : '' }}"
+                                                            href="{{ route('web_kids') }}">
                                                             KIDS
                                                         </a>
                                                     </li>
                                                     <li class="dropdown">
-                                                        <a class="dropdown-item {{ request()->routeIs('web_testimonios') ? 'active' : '' }}" href="{{ route('web_testimonios') }}">
+                                                        <a class="dropdown-item {{ request()->routeIs('web_testimonios') ? 'active' : '' }}"
+                                                            href="{{ route('web_testimonios') }}">
                                                             TESTIMONIOS
                                                         </a>
                                                     </li>
                                                     <li class="dropdown">
-                                                        <a class="dropdown-item {{ request()->routeIs('web_contacto') ? 'active' : '' }}" href="{{ route('web_contacto') }}">
+                                                        <a class="dropdown-item {{ request()->routeIs('web_contacto') ? 'active' : '' }}"
+                                                            href="{{ route('web_contacto') }}">
                                                             CONTACTO
                                                         </a>
                                                     </li>
                                                     <li class="dropdown">
-                                                        <a class="dropdown-item {{ request()->routeIs('web_donar') ? 'active' : '' }}" href="{{ route('web_donar') }}">
+                                                        <a class="dropdown-item {{ request()->routeIs('web_donar') ? 'active' : '' }}"
+                                                            href="{{ route('web_donar') }}">
                                                             DONAR
                                                         </a>
                                                     </li>
                                                 </ul>
                                             </nav>
                                         </div>
-                                        <button class="btn header-btn-collapse-nav" data-bs-toggle="collapse" data-bs-target=".header-nav-main nav">
+                                        <button class="btn header-btn-collapse-nav" data-bs-toggle="collapse"
+                                            data-bs-target=".header-nav-main nav">
                                             <i class="fas fa-bars"></i>
                                         </button>
                                     </div>
