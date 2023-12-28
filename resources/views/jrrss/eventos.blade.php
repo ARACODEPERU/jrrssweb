4fb6fd2dcb9403997b481a61d1473dd2f47f3e69
Yisus Web <connexion.jesus@gmail.com>
2023-12-28 17:45:50 -0500

    ghu

--- resources/views/jrrss/eventos.blade.php
+++ resources/views/jrrss/eventos.blade.php
@@ -127,14 +127,14 @@
                                             <div class="row">
                                                 <div class="form-group col">
                                                     <label for="exampleDataList" class="form-label">Datalist example</label>
-<input class="form-control" list="datalistOptions" id="exampleDataList" placeholder="Type to search...">
-<datalist id="datalistOptions">
-  <option value="San Francisco">
-  <option value="New York">
-  <option value="Seattle">
-  <option value="Los Angeles">
-  <option value="Chicago">
-</datalist>
+                                                        <input class="form-control" list="datalistOptions" id="exampleDataList" placeholder="Type to search...">
+                                                        <datalist id="datalistOptions">
+                                                            <option value="San Francisco">
+                                                            <option value="New York">
+                                                            <option value="Seattle">
+                                                            <option value="Los Angeles">
+                                                            <option value="Chicago">
+                                                        </datalist>
                                                     <select class="form-select form-control bg-color-tertiary"
                                                         name="ubigeo" id="ubigeo" required>
                                                         <option selected>Desde donde viene?</option>
