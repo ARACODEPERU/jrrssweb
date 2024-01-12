4e0f16844dd85f0196d25a2323fb54b7fb4c1af2
Yisus Web <connexion.jesus@gmail.com>
2024-01-12 00:06:05 -0500

    hj

--- resources/views/components/jrrss/header-area.blade.php
+++ resources/views/components/jrrss/header-area.blade.php
@@ -270,13 +270,13 @@
     <!-- Modal -->
     @if($transmissions)
     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-dialog modal-lg">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLabel">{{ $transmissions->title }}</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div class="modal-body">
+                <div class="modal-body" style="text-align: center;">
                     {!! $transmissions->iframe_transmission !!}
                 </div>
                 <div class="modal-footer">
