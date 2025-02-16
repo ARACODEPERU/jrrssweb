ecb6dd6fd993f42933a8a495ea58a0251dd16c67
José Carlos <60234854+Grandejosh@users.noreply.github.com>
2025-02-15 20:52:37 -0500

    s
    s

--- app/Mail/ConfirmTicketEventMailable.php
+++ app/Mail/ConfirmTicketEventMailable.php
@@ -9,6 +9,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Mail\Mailables\Address;
+use App\Models\EvenEvent;
 
 class ConfirmTicketEventMailable extends Mailable
 {
