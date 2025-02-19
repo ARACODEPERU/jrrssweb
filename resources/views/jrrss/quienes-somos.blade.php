876cb6e272cbe4bc69d1fe254e1af453cd17e5a8
Yisus Web <connexion.jesus@gmail.com>
2025-02-19 13:35:08 -0500

    vision

--- resources/views/jrrss/quienes-somos.blade.php
+++ resources/views/jrrss/quienes-somos.blade.php
@@ -34,7 +34,7 @@
 
 
 
-            <div class="container-lg pt-4">
+            {{-- <div class="container-lg pt-4">
                 <div class="row">
                     <div class="col">
                         <h4 class="mb-2 mt-5">{{ $visions[0]->content }}</h4>
@@ -87,7 +87,7 @@
 
                     </div>
                 </div>
-            </div>
+            </div> --}}
 
 
 
