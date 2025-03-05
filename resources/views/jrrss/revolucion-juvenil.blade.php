e65efb29b4f17e8606e61a933ddf28950ae051c0
Yisus Web <connexion.jesus@gmail.com>
2025-03-05 13:46:59 -0500

    rt6uyu

--- resources/views/jrrss/revolucion-juvenil.blade.php
+++ resources/views/jrrss/revolucion-juvenil.blade.php
@@ -43,36 +43,32 @@
             <br><br>
 
             <div class="container-lg py-4">
-                <div class="row">
-                    <div class="col" style="min-height: 250px;">
-                        <div class="row portfolio-list lightbox" 
-                        data-plugin-options="{'delegate': 'a.lightbox-portfolio', 'type': 'image', 'gallery': {'enabled': true}}">
-                            @foreach ($galeryRmnt as $item)
-                                <div class="col-md-4 appear-animation animated expandIn appear-animation-visible"
-                                    data-appear-animation="expandIn" data-appear-animation-delay="1000"
-                                    style="animation-delay: 100ms;">
-                                    <div class="portfolio-item">
-                                        <span
-                                            class="thumb-info thumb-info-lighten thumb-info-centered-icons border-radius-0">
-                                            <span class="thumb-info-wrapper border-radius-0">
-                                                <img src="{{ $item->group->items[0]->content }}"
-                                                    class="img-fluid border-radius-0" alt="">
-                                                <span class="thumb-info-action">
-                                                    <a href="{{ $item->group->items[0]->content }}"
-                                                        class="lightbox-portfolio">
-                                                        <span class="thumb-info-action-icon thumb-info-action-icon-light"><i
-                                                                class="fas fa-search text-dark"></i></span>
-                                                    </a>
-                                                </span>
-                                            </span>
+                <div class="row portfolio-list lightbox" 
+                    data-plugin-options="{'delegate': 'a.lightbox-portfolio', 'type': 'image', 'gallery': {'enabled': true}}">
+                    @foreach ($galeryRmnt as $item)
+                        <div class="col-md-4 appear-animation animated expandIn appear-animation-visible"
+                            data-appear-animation="expandIn" data-appear-animation-delay="1000"
+                            style="animation-delay: 100ms;">
+                            <div class="portfolio-item">
+                                <span
+                                    class="thumb-info thumb-info-lighten thumb-info-centered-icons border-radius-0">
+                                    <span class="thumb-info-wrapper border-radius-0">
+                                        <img src="{{ $item->group->items[0]->content }}"
+                                            class="img-fluid border-radius-0" alt="">
+                                        <span class="thumb-info-action">
+                                            <a href="{{ $item->group->items[0]->content }}"
+                                                class="lightbox-portfolio">
+                                                <span class="thumb-info-action-icon thumb-info-action-icon-light"><i
+                                                        class="fas fa-search text-dark"></i></span>
+                                            </a>
                                         </span>
-                                    </div>
-                                </div>
-                            @endforeach
+                                    </span>
+                                </span>
+                            </div>
                         </div>
-
-                    </div>
+                    @endforeach
                 </div>
+
                 <div class="row">
                     <div class="col-md-4"></div>
                     <div class="col-md-4">
@@ -150,7 +146,7 @@
 
                         <div class="row portfolio-list lightbox" data-plugin-options="{'delegate': 'a.lightbox-portfolio', 'type': 'image', 'gallery': {'enabled': true}}">
                             @foreach ($videoteca as $video)
-                                <div class="col-md-6" style="padding: 15px;">
+                                <div class="col-md-4" style="padding: 15px;">
                                     {!! $video->group->items[0]->content !!}
                                 </div>
                             @endforeach
