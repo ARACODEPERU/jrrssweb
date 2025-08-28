1793b36f64cc7659c423df180ae38bfd8f950fe4
ElRodriguez <elrodriguez2423@github.com>
2025-08-28 13:50:30 -0500

    cambios

--- Modules/Socialevents/Resources/assets/js/Menu.js
+++ Modules/Socialevents/Resources/assets/js/Menu.js
@@ -1,10 +1,16 @@
-import { faMasksTheater } from "@fortawesome/free-solid-svg-icons";
+import {
+    faMasksTheater,
+    faLayerGroup,
+    faBuildingFlag,
+    faElevator,
+    faTicket
+} from "@fortawesome/free-solid-svg-icons";
 
 const menuSocialevents = {
     status: false,
     text: "Eventos",
     icom: faMasksTheater,
-    route: null,
+    route: 'module',
     permissions: "even_dashboard",
     items: [
         {
@@ -12,24 +18,28 @@ const menuSocialevents = {
             status: false,
             text: "Categorías",
             permissions: "even_categoria_listado",
+            icom: faLayerGroup,
         },
         {
             route: route("even_local_list"),
             status: false,
             text: "Locales",
             permissions: "even_local_listado",
+            icom: faBuildingFlag,
         },
         {
             route: route("even_eventos_list"),
             status: false,
             text: "Eventos",
             permissions: "even_evento_listado",
+            icom: faElevator,
         },
         {
             route: route("even_tickets_listado"),
             status: false,
             text: "Ticket Vendidos",
             permissions: "even_ventas_listado",
+            icom: faTicket,
         },
     ],
 };
