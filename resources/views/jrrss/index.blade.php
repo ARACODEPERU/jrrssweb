223dcb9ae14348173720980f8fc4765cbd71fe95
Yisus Web <connexion.jesus@gmail.com>
2026-01-29 11:33:06 -0500

    dfdf

--- resources/views/jrrss/index.blade.php
+++ resources/views/jrrss/index.blade.php
@@ -15,7 +15,8 @@
                 dots-inside dots-vertical-center dots-align-right dots-orientation-portrait custom-dots-style-1 show-dots-hover 
                 show-dots-xs nav-style-1 nav-inside nav-inside-plus nav-dark nav-lg nav-font-size-lg show-nav-hover mb-0 view-pc" 
                 data-plugin-options="{'autoplayTimeout': 6500}" 
-                data-dynamic-height="['650px','650px','650px','550px','500px']"
+                data-dynamic-height="['650px','650px','650px','550px','500px']">
+                <div class="owl-stage-outer">
                     <div class="owl-stage ara_centrado_total">
                         @foreach ($sliders as $slide)
                         <div  class="owl-item position-relative " 
@@ -25,6 +26,7 @@
                         </div>
                         @endforeach
                     </div>
+                </div>
                 </div>
                 <div class="owl-dots mb-5">
                     @foreach ($sliders as $key => $slide)
@@ -70,12 +72,12 @@
                                                 data-appear-animation="fadeInUpShorter" data-appear-animation-delay="1300">
                                                 {{ $reuniones[0]->item->content ?? 'Reuniones' }}
                                     </h2>
-                                    <h3 class="text-9 line-height-3 text-transf 
+                                    <h3 class="text-9 line-height-3 text-transform-none font-weight-semibold mb-4 appear-animation"
                                         data-appear-animation="fadeInUpShorter" data-appear-animation-delay="1500">
                                         {{ $reuniones[5]->item->content ?? '' }}
                                     </h3>
                                     <p class="pb-3 mb-4 appear-animation"
-                                        data-appear-animation="fadeInUp 0">
+                                        data-appear-animation="fadeInUpShorter" data-appear-animation-delay="1900">
                                         {{ $reuniones[6]->item->content ?? '' }}
                                     </p>
                                 </div>
@@ -89,12 +91,13 @@
                                             <div class="img">
                                                 <img src="{{ asset('storage/' . ($reunion->item->items[1]->content ?? 'default.jpg')) }}" alt="{{ $reunion->item->items[0]->content ?? 'Imagen' }}">
                                             </div>
-                                            <div class="info"> 
-                                                <br>
-                                                <h4 class="
+                                            <div class="info">
+                                                <h4 class="p-2"><b>{{ $reunion->item->items[2]->content ?? '' }}: {{ $reunion->item->items[3]->content ?? '' }}</b> </h4>
+                                                <p class="mt-n4">
                                                     <b>Horario de Reunión:</b> {{ $reunion->item->items[5]->content ?? '' }}
                                                 </p>
                                             </div>
+                                        </a>
                                     </div>
                                 </div>
                         @endforeach
@@ -111,7 +114,8 @@
                                     {{ $bible[1]->content ?? '' }}
                                 </h1>
                                 <p class="text-color-light opacity-7 text-3 px-5 mx-5 mb-0 appear-animation"
-                                    data-appear-animation="fadeInUpShorter" data-appear-animation-delay="400">[
+                                    data-appear-animation="fadeInUpShorter" data-appear-animation-delay="400">
+                                    {{ $bible[2]->content ?? '' }}
                                 </p>
                             </div>
                         </div>
@@ -122,7 +126,8 @@
                     <div class="row">
                         <div class="container mt-4">
                             <div class="row justify-content-center">
-                                <div class="col-lg-11 col light-text-1 d-inline-block line-height-5
+                                <div class="col-md-12 text-center">
+                                    <h2 class="custom-highlight-text-1 d-inline-block line-height-5
                                                 text-4 positive-ls-3 font-weight-medium text-color-primary
                                                 mb-2 appear-animation"
                                                 data-appear-animation="fadeInUpShorter" data-appear-animation-delay="1300">
@@ -144,12 +149,13 @@
                     <div class="row">
                         @foreach ($ministerios->slice(1, 3) as $key => $ministerio)
                             <div class="col-md-4 p-2">
-                                <div class="ih-item square colored effect ]->content ?? '#' }}">
+                                <div class="ih-item square colored effect8 scale_down">
+                                    <a href="{{ $ministerio->item->items[3]->content ?? '#' }}">
                                         <div class="img">
                                             <img src="{{ asset('storage/' . ($ministerio->item->items[0]->content ?? '')) }}" alt="{{ $ministerio->item->items[1]->content ?? '' }}">
                                         </div>
                                         <div class="info">
-                                            <h3>{{ $ministerio->item->ite 
+                                            <h3>{{ $ministerio->item->items[1]->content ?? '' }}</h3>
                                             <p class="mt-n4">
                                                 <b>{{ $ministerio->item->items[2]->content ?? '' }}</b>
                                             </p>
@@ -159,15 +165,17 @@
                                 </div>
                             </div>
                         @endforeach
-                    </div>>
+                    </div>
             </section>
             <section class="section section-parallax section-height-5 overlay overlay-show overlay-op-7 border-0 m-0"
                     data-appear-animation="fadeIn" data-plugin-parallax data-plugin-options="{'speed': 1.5, 'parallaxHeight': '138%'}"
                     data-image-src="{{ asset('themes/jrrss/assets/img/parallax/parallax_index.jpg') }}">
                     <div class="container"> 
+                        <div class="row justify-content-center">
                             <div class="col-lg-9 text-center">
                                 <h1 class="text-color-light font-weight-bold custom-tertiary-font ls-0 mb-4 appear-animation font-montserrat"
-                                    data-appear-animation="fadeInUpShorter" data-appear-an 
+                                    data-appear-animation="fadeInUpShorter" data-appear-animation-delay="200">
+                                    {{ $gods_meeting[0]->content ?? '' }}
                                 </h1>
                                 <p class="text-color-light opacity-7 px-5 mx-5 mb-0 appear-animation fs-5"
                                     data-appear-animation="fadeInUpShorter" data-appear-animation-delay="400">
@@ -273,12 +281,10 @@
                             </div>
                         </div>
                     </div>
+                    @endforeach
 
 
         </div>
-o
-
-
 
     </div>
 
