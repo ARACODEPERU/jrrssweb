763b32e3a8e27b51ad1fa34e8ca0418f2413ca17
Yisus Web <connexion.jesus@gmail.com>
2026-01-29 11:04:06 -0500

    sdjsd

--- resources/views/jrrss/index.blade.php
+++ resources/views/jrrss/index.blade.php
@@ -15,9 +15,7 @@
                 dots-inside dots-vertical-center dots-align-right dots-orientation-portrait custom-dots-style-1 show-dots-hover 
                 show-dots-xs nav-style-1 nav-inside nav-inside-plus nav-dark nav-lg nav-font-size-lg show-nav-hover mb-0 view-pc" 
                 data-plugin-options="{'autoplayTimeout': 6500}" 
-                data-dynamic-height="['650px','650px','650px','550px','500px']" 
-                style="height: 650px; background:#fff;">
-                <div class="owl-stage-outer">
+                data-dynamic-height="['650px','650px','650px','550px','500px']"
                     <div class="owl-stage ara_centrado_total">
                         @foreach ($sliders as $slide)
                         <div  class="owl-item position-relative " 
@@ -70,104 +68,90 @@
                                                 text-4 positive-ls-3 font-weight-medium text-color-primary
                                                 mb-2 appear-animation"
                                                 data-appear-animation="fadeInUpShorter" data-appear-animation-delay="1300">
-                                                {{ $reuniones[0]->item->content }}
+                                                {{ $reuniones[0]->item->content ?? 'Reuniones' }}
                                     </h2>
-                                    <h3 class="text-9 line-height-3 text-transform-none font-weight-semibold mb-4
-                                        appear-animation"
+                                    <h3 class="text-9 line-height-3 text-transf 
                                         data-appear-animation="fadeInUpShorter" data-appear-animation-delay="1500">
-                                        {{ $reuniones[5]->item->content }}
+                                        {{ $reuniones[5]->item->content ?? '' }}
                                     </h3>
                                     <p class="pb-3 mb-4 appear-animation"
-                                        data-appear-animation="fadeInUpShorter"
-                                        data-appear-animation-delay="1900">
-                                        {{ $reuniones[6]->item->content }}
+                                        data-appear-animation="fadeInUp 0">
+                                        {{ $reuniones[6]->item->content ?? '' }}
                                     </p>
                                 </div>
                             </div>
-                        </div>
-                    </div>
+                        </div> 
                     <div class="row">
                         @foreach ($reuniones->slice(1, 4) as $key => $reunion)
                                 <div class="col-md-6 p-3">
                                     <div class="ih-item square colored effect8 scale_down">
-                                        <a href="" data-bs-toggle="modal" data-bs-target="#reuniones{{ $key }}">
+                                        <a href="#" data-bs-toggle="modal" data-bs-target="#reuniones{{ $key }}">
                                             <div class="img">
-                                                <img src="{{ asset('storage/' . $reunion->item->items[1]->content) }}" alt="{{ $reunion->item->items[0]->content }}">
+                                                <img src="{{ asset('storage/' . ($reunion->item->items[1]->content ?? 'default.jpg')) }}" alt="{{ $reunion->item->items[0]->content ?? 'Imagen' }}">
                                             </div>
-                                            <div class="info">
-                                                <h3>{{ $reunion->item->items[0]->content }}</h3>
+                                            <div class="info"> 
                                                 <br>
-                                                <h4 class="p-2"><b>{{ $reunion->item->items[2]->content }}: {{ $reunion->item->items[3]->content }}</b> </h4>
-                                                <p class="mt-n4">
-                                                    <b>Horario de Reunión:</b> {{ $reunion->item->items[5]->content }}
+                                                <h4 class="
+                                                    <b>Horario de Reunión:</b> {{ $reunion->item->items[5]->content ?? '' }}
                                                 </p>
                                             </div>
-                                        </a>
                                     </div>
                                 </div>
                         @endforeach
-                    </div>
                 </div>
             </section>
-
             <section class="section section-parallax section-height-5 overlay overlay-show overlay-op-7 border-0 m-0"
                     data-appear-animation="fadeIn" data-plugin-parallax data-plugin-options="{'speed': 1.5, 'parallaxHeight': '138%'}"
-                    data-image-src="{{ asset('storage/' . $bible[0]->content) }}">
+                    data-image-src="{{ asset('storage/' . ($bible[0]->content ?? '')) }}">
                     <div class="container">
                         <div class="row justify-content-center">
                             <div class="col-lg-9 text-center">
                                 <h1 class="text-color-light font-weight-bold custom-tertiary-font ls-0 mb-4 appear-animation font-montserrat"
                                     data-appear-animation="fadeInUpShorter" data-appear-animation-delay="200">
-                                    {{ $bible[1]->content }}
+                                    {{ $bible[1]->content ?? '' }}
                                 </h1>
                                 <p class="text-color-light opacity-7 text-3 px-5 mx-5 mb-0 appear-animation"
-                                    data-appear-animation="fadeInUpShorter" data-appear-animation-delay="400">
-                                    {{ $bible[2]->content }}
+                                    data-appear-animation="fadeInUpShorter" data-appear-animation-delay="400">[
                                 </p>
                             </div>
                         </div>
                     </div>
             </section>
 
-            <section class="py-5 my-4">
-                <div class="container-lg">
+            <section class="py-5 my-4"> 
                     <div class="row">
                         <div class="container mt-4">
                             <div class="row justify-content-center">
-                                <div class="col-lg-11 col-xl-10 text-center">
-                                    <h2 class="custom-highlight-text-1 d-inline-block line-height-5
+                                <div class="col-lg-11 col light-text-1 d-inline-block line-height-5
                                                 text-4 positive-ls-3 font-weight-medium text-color-primary
                                                 mb-2 appear-animation"
                                                 data-appear-animation="fadeInUpShorter" data-appear-animation-delay="1300">
-                                                {{ $ministerios[0]->item->content }}
+                                                {{ $ministerios[0]->item->content ?? 'Ministerios' }}
                                     </h2>
                                     <h3 class="text-9 line-height-3 text-transform-none font-weight-semibold mb-4
                                         appear-animation"
                                         data-appear-animation="fadeInUpShorter" data-appear-animation-delay="1500">
-                                        {{ $ministerios[4]->item->content }}
+                                        {{ $ministerios[4]->item->content ?? '' }}
                                     </h3>
                                     <p class="text-3-5 pb-3 mb-4 appear-animation"
                                         data-appear-animation="fadeInUpShorter"
                                         data-appear-animation-delay="1900">
-                                        {{ $ministerios[5]->item->content }}
+                                        {{ $ministerios[5]->item->content ?? '' }}
                                     </p>
                                 </div>
-                            </div>
                         </div>
                     </div>
                     <div class="row">
                         @foreach ($ministerios->slice(1, 3) as $key => $ministerio)
                             <div class="col-md-4 p-2">
-                                <div class="ih-item square colored effect8 scale_down">
-                                    <a href="{{ $ministerio->item->items[3]->content }}">
+                                <div class="ih-item square colored effect ]->content ?? '#' }}">
                                         <div class="img">
-                                            <img src="{{ asset('storage/' . $ministerio->item->items[0]->content) }}" alt="{{ $ministerio->item->items[1]->content }}">
+                                            <img src="{{ asset('storage/' . ($ministerio->item->items[0]->content ?? '')) }}" alt="{{ $ministerio->item->items[1]->content ?? '' }}">
                                         </div>
                                         <div class="info">
-                                            <h3>{{ $ministerio->item->items[1]->content }}</h3>
-                                            <br>
+                                            <h3>{{ $ministerio->item->ite 
                                             <p class="mt-n4">
-                                                <b>{{ $ministerio->item->items[2]->content }}</b>
+                                                <b>{{ $ministerio->item->items[2]->content ?? '' }}</b>
                                             </p>
                                             <button class="btn btn-dark">Ingresar</button>
                                         </div>
@@ -175,19 +159,15 @@
                                 </div>
                             </div>
                         @endforeach
-                    </div>
-                </div>
+                    </div>>
             </section>
-
             <section class="section section-parallax section-height-5 overlay overlay-show overlay-op-7 border-0 m-0"
                     data-appear-animation="fadeIn" data-plugin-parallax data-plugin-options="{'speed': 1.5, 'parallaxHeight': '138%'}"
                     data-image-src="{{ asset('themes/jrrss/assets/img/parallax/parallax_index.jpg') }}">
-                    <div class="container">
-                        <div class="row justify-content-center">
+                    <div class="container"> 
                             <div class="col-lg-9 text-center">
                                 <h1 class="text-color-light font-weight-bold custom-tertiary-font ls-0 mb-4 appear-animation font-montserrat"
-                                    data-appear-animation="fadeInUpShorter" data-appear-animation-delay="200">
-                                    {{ $gods_meeting[0]->content }}
+                                    data-appear-animation="fadeInUpShorter" data-appear-an 
                                 </h1>
                                 <p class="text-color-light opacity-7 px-5 mx-5 mb-0 appear-animation fs-5"
                                     data-appear-animation="fadeInUpShorter" data-appear-animation-delay="400">
@@ -273,17 +253,17 @@
                         <div class="modal-dialog modal-dialog-centered">
                             <div class="modal-content">
                                 <div class="modal-header">
-                                    <h4 class="modal-title" id="reunionesLabel{{ $key }}">{{ $reunion->item->items[0]->content }}</h4>
+                                    <h4 class="modal-title" id="reunionesLabel{{ $key }}">{{ $reunion->item->items[0]->content ?? '' }}</h4>
                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                 </div>
                                 <div class="modal-body">
-                                    <h5><b>{{ $reunion->item->items[2]->content }}: {{ $reunion->item->items[3]->content }}</b> </h5>
-                                    <p style="margin-top: -15px;">
-                                        <b>Horario de Reunión:</b> {{ $reunion->item->items[5]->content }}
+                                    <h5><b>{{ $reunion->item->items[2]->content ?? '' }}: {{ $reunion->item->items[3]->content ?? '' }}</b> </h5>
+                                    <p class="mt-n3">
+                                        <b>Horario de Reunión:</b> {{ $reunion->item->items[5]->content ?? '' }}
                                     </p>
                                     <div class="row">
                                         <div class="col-md-12">
-                                            {!!  $reunion->item->items[4]->content  !!}
+                                            {!!  $reunion->item->items[4]->content ?? ''  !!}
                                         </div>
                                     </div>
                                 </div>
@@ -293,14 +273,10 @@
                             </div>
                         </div>
                     </div>
-            @endforeach
 
 
         </div>
-
-        <!-- Footer - area start -->
-        <x-jrrss.footer-area></x-jrrss.footer-area>
-        <!-- Footer - area end -->
+o
 
 
 
--- resources/views/jrrss/quienes-somos.blade.php
+++ resources/views/jrrss/quienes-somos.blade.php
@@ -14,13 +14,13 @@
             <section data-plugin-parallax data-plugin-options="{'speed': 1.2}" class="view-pc"
                 style="position: relative; height: 310px; overflow: hidden;">
                 <img style="width: 100%; height: auto; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);"
-                    src="{{ $banner->content }}" alt="">
+                    src="{{ $banner->content ?? '' }}" alt="Banner Quiénes Somos">
             </section>
 
             <section data-plugin-parallax data-plugin-options="{'speed': 1.2}" class="view-movile"
                 style="position: relative; height: 80px; overflow: hidden;">
                 <img style="width: 100%; height: auto; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);"
-                    src="{{ $banner->content }}" alt="">
+                    src="{{ $banner->content ?? '' }}" alt="Banner Quiénes Somos">
             </section>
 
             {{-- <section data-plugin-parallax data-plugin-options="{'speed': 1.2}">
@@ -32,80 +32,22 @@
                     <div class="col-md-6">
                         <br>
                         <h2>
-                            <b  style="font-weight: 600;">{{ $presentacion[0]->content }}</b>
+                            <b  style="font-weight: 600;">{{ $presentacion[0]->content ?? 'Quiénes Somos' }}</b>
                         </h2>
                         <p style="padding: 0px;">
-                            {{ $presentacion[1]->content }}
+                            {{ $presentacion[1]->content ?? '' }}
                         </p>
                     </div>
                     <div class="col-md-6 ara_centrado_total">
-                        <img width="100%;" src="{{ $presentacion[2]->content }}" alt="">
+                        <img width="100%;" src="{{ $presentacion[2]->content ?? '' }}" alt="Presentación">
                     </div>
                 </div>
             </div>
 
 
-
-            {{-- <div class="container-lg pt-4">
-                <div class="row">
-                    <div class="col">
-                        <h4 class="mb-2 mt-5">{{ $visions[0]->content }}</h4>
-
-                        <div class="row process process-connecting-line my-5">
-                            <div class="connecting-line appear-animation animated fadeIn appear-animation-visible" data-appear-animation="fadeIn" style="animation-delay: 100ms;"></div>
-                            <div class="col-md-3 mb-5 mb-lg-4 appear-animation animated fadeInRightShorter appear-animation-visible" data-appear-animation="fadeInRightShorter" data-appear-animation-delay="800" style="animation-delay: 800ms;">
-                                <div class="process-step-circle">
-                                    <strong class="process-step-circle-content">{{ $visions[1]->content }}</strong>
-                                </div>
-                                <div class="process-step-content">
-                                    <h4 class="mb-1 text-4 font-weight-bold">{{ $visions[2]->content }}</h4>
-                                    <p class="mb-0">
-                                        {{ $visions[3]->content }}
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="col-md-3 mb-5 mb-lg-4 appear-animation animated fadeInRightShorter appear-animation-visible" data-appear-animation="fadeInRightShorter" data-appear-animation-delay="900" style="animation-delay: 900ms;">
-                                <div class="process-step-circle">
-                                    <strong class="process-step-circle-content">{{ $visions[4]->content }}</strong>
-                                </div>
-                                <div class="process-step-content">
-                                    <h4 class="mb-1 text-4 font-weight-bold">{{ $visions[5]->content }}</h4>
-                                    <p class="mb-0">{{ $visions[6]->content }}</p>
-                                </div>
-                            </div>
-                            <div class="col-md-3 mb-5 mb-lg-4 appear-animation animated fadeInRightShorter appear-animation-visible" data-appear-animation="fadeInRightShorter" data-appear-animation-delay="1000" style="animation-delay: 1000ms;">
-                                <div class="process-step-circle">
-                                    <strong class="process-step-circle-content">{{ $visions[7]->content }}</strong>
-                                </div>
-                                <div class="process-step-content">
-                                    <h4 class="mb-1 text-4 font-weight-bold">{{ $visions[8]->content }}</h4>
-                                    <p class="mb-0">
-                                        {{ $visions[9]->content }}
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="col-md-3 mb-5 mb-lg-4 appear-animation animated fadeInRightShorter appear-animation-visible" data-appear-animation="fadeInRightShorter" data-appear-animation-delay="1000" style="animation-delay: 1000ms;">
-                                <div class="process-step-circle">
-                                    <strong class="process-step-circle-content">{{ $visions[10]->content }}</strong>
-                                </div>
-                                <div class="process-step-content">
-                                    <h4 class="mb-1 text-4 font-weight-bold">{{ $visions[11]->content }}</h4>
-                                    <p class="mb-0">
-                                        {{ $visions[12]->content }}
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-            </div> --}}
-
-
-
             <section class="section section-parallax section-height-5 overlay overlay-show overlay-op-7 border-0 m-0 appear-animation animated fadeIn appear-animation-visible"
                     data-appear-animation="fadeIn" data-plugin-parallax="" data-plugin-options="{'speed': 1.5, 'parallaxHeight': '138%'}"
-                    data-image-src="{{ $parallax[0]->content }}" style="position: relative; overflow: hidden; animation-delay: 100ms;">
+                    data-image-src="{{ $parallax[0]->content ?? '' }}" style="position: relative; overflow: hidden; animation-delay: 100ms;">
                     <div class="container">
                         <div class="row justify-content-center">
                             <div class="col-md-2"></div>
@@ -113,11 +55,11 @@
                                 <h2 class="text-color-light font-weight-bold custom-tertiary-font mb-4 appear-animation animated fadeInUpShorter appear-animation-visible"
                                     data-appear-animation="fadeInUpShorter" data-appear-animation-delay="200" 
                                     style="animation-delay: 200ms; font-size: 60px; line-height: 60px; font-family: 'Montserrat', serif !important; ">
-                                    {{ $parallax[1]->content }}
+                                    {{ $parallax[1]->content ?? '' }}
                                 </h2>
                                 <p class="text-color-light opacity-7 text-3 px-5 mx-5 mb-0 appear-animation animated fadeInUpShorter appear-animation-visible"
                                     data-appear-animation="fadeInUpShorter" data-appear-animation-delay="400" style="animation-delay: 400ms;">
-                                    {{ $parallax[2]->content }}
+                                    {{ $parallax[2]->content ?? '' }}
                                 </p>
                             </div>
                             <div class="col-md-2"></div>
@@ -128,30 +70,26 @@
             <div class="container-lg pt-4">
                 <div class="row">
                     <div class="col">
-                        <h4 class="mb-2 mt-5">{{ $resenas[0]->item->content }}</h4>
+                        <h4 class="mb-2 mt-5">{{ $resenas[0]->item->content ?? 'Reseña Histórica' }}</h4>
 
                         <div class="process process-vertical py-4">
-                            @foreach ($resenas as $key => $resena)
-                                @if ($key>0)
-                                    <div class="process-step appear-animation animated fadeInUpShorter appear-animation-visible" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="200" style="animation-delay: 200ms;">
-                                        <div class="process-step-circle">
-                                            <strong class="process-step-circle-content">{{ $resena->item->items[0]->content }}</strong>
-                                        </div>
-                                        <div class="process-step-content">
-                                            <h4 class="mb-1 text-4 font-weight-bold">{{ $resena->item->items[1]->content }}</h4>
-                                            <p class="mb-0">{{ $resena->item->items[2]->content }}</p>
-                                        </div>
+                            {{-- Usamos slice(1) para saltar el título y evitar el if dentro del loop --}}
+                            @foreach ($resenas->slice(1) as $resena)
+                                <div class="process-step appear-animation animated fadeInUpShorter appear-animation-visible" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="200" style="animation-delay: 200ms;">
+                                    <div class="process-step-circle">
+                                        <strong class="process-step-circle-content">{{ $resena->item->items[0]->content ?? '' }}</strong>
                                     </div>
-                                    @endif
+                                    <div class="process-step-content">
+                                        <h4 class="mb-1 text-4 font-weight-bold">{{ $resena->item->items[1]->content ?? '' }}</h4>
+                                        <p class="mb-0">{{ $resena->item->items[2]->content ?? '' }}</p>
+                                    </div>
+                                </div>
                             @endforeach
                         </div>
 
                     </div>
                 </div>
             </div>
-
-
-
 
         </div>
 
