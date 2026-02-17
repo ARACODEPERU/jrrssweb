15d3d6e09bb92ae49a28c74d7bb2053a2a371d20
Yisus Web <connexion.jesus@gmail.com>
2026-02-17 18:22:36 -0500

    ghvb

--- resources/views/jrrss/quienes-somos.blade.php
+++ resources/views/jrrss/quienes-somos.blade.php
@@ -4,6 +4,9 @@
 
     <div class="body">
 
+        <!-- AOS Animation CSS -->
+        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
         <!-- Header - area start -->
         <x-jrrss.header-area></x-jrrss.header-area>
         <!-- Header - area end -->
@@ -11,13 +14,13 @@
 
         <div role="main" class="main">
 
-            <section data-plugin-parallax data-plugin-options="{'speed': 1.2}" class="view-pc"
+            <section data-plugin-parallax data-plugin-options="{'speed': 1.2}" class="view-pc" data-aos="fade-in"
                 style="position: relative; height: 310px; overflow: hidden;">
                 <img style="width: 100%; height: auto; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);"
                     src="{{ asset('storage/' . $banner->content ?? '') }}" alt="Banner Quiénes Somos">
             </section>
 
-            <section data-plugin-parallax data-plugin-options="{'speed': 1.2}" class="view-movile"
+            <section data-plugin-parallax data-plugin-options="{'speed': 1.2}" class="view-movile" data-aos="fade-in"
                 style="position: relative; height: 80px; overflow: hidden;">
                 <img style="width: 100%; height: auto; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);"
                     src="{{ asset('storage/' . $banner->content ?? '') }}" alt="Banner Quiénes Somos">
@@ -25,7 +28,7 @@
 
             <div class="container-lg" style="padding: 30px 20px 60px 20px;">
                 <div class="row">
-                    <div class="col-md-6">
+                    <div class="col-md-6" data-aos="fade-right" data-aos-delay="200">
                         <br>
                         <h2>
                             <b  style="font-weight: 600;">{{ $presentacion[0]->content ?? 'Quiénes Somos' }}</b>
@@ -34,7 +37,7 @@
                             {{ $presentacion[1]->content ?? '' }}
                         </p>
                     </div>
-                    <div class="col-md-6 ara_centrado_total">
+                    <div class="col-md-6 ara_centrado_total" data-aos="fade-left" data-aos-delay="400">
                         <img style="width: 100%;" src="{{ asset('storage/' . ($presentacion[2]->content ?? '')) }}" alt="Presentación">
                     </div>
                 </div>
@@ -83,7 +86,7 @@
                 <div class="container">
                     <div class="row justify-content-center">
                         <div class="col-lg-10 col-xl-8">
-                            <div class="glass-card text-center appear-animation" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="200">
+                            <div class="glass-card text-center" data-aos="zoom-in" data-aos-delay="200">
                                 <h2 class="text-white font-weight-bold text-9 mb-3" style="text-shadow: 2px 2px 4px rgba(0,0,0,0.5); font-family: 'Montserrat', serif !important;">
                                     {{ $parallax[1]->content ?? '' }}
                                 </h2>
@@ -103,12 +106,12 @@
             <div class="container-lg pt-4">
                 <div class="row">
                     <div class="col">
-                        <h4 class="mb-2 mt-5">{{ $resenas[0]->item->content ?? 'Reseña Histórica' }}</h4>
+                        <h4 class="mb-2 mt-5" data-aos="fade-up">{{ $resenas[0]->item->content ?? 'Reseña Histórica' }}</h4>
 
                         <div class="process process-vertical py-4">
                             {{-- Usamos slice(1) para saltar el título y evitar el if dentro del loop --}}
                             @foreach ($resenas->slice(1) as $resena)
-                                <div class="process-step appear-animation animated fadeInUpShorter appear-animation-visible" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="200" style="animation-delay: 200ms;">
+                                <div class="process-step" data-aos="fade-up" data-aos-delay="{{ $loop->iteration * 150 }}">
                                     <div class="process-step-circle">
                                         <strong class="process-step-circle-content">{{ $resena->item->items[0]->content ?? '' }}</strong>
                                     </div>
@@ -134,4 +137,12 @@
 
     </div>
 
+    <!-- AOS Animation JS -->
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+        AOS.init({
+            mirror: true, // Animación al hacer scroll hacia arriba
+            once: false, // Animación cada vez que se hace scroll
+        });
+    </script>
 @endsection
