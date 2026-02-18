27b92d8bf85ea971f8c7cbba3650b76b02c4187c
Yisus Web <connexion.jesus@gmail.com>
2026-02-18 00:39:55 -0500

    ghftyty

--- resources/views/jrrss/ecelt.blade.php
+++ resources/views/jrrss/ecelt.blade.php
@@ -3,6 +3,9 @@
 @section('content')
     <div class="body">
 
+        <!-- AOS Animation CSS -->
+        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
         <!-- Header - area start -->
         <x-jrrss.header-area></x-jrrss.header-area>
         <!-- Header - area end -->
@@ -10,13 +13,13 @@
 
         <div role="main" class="main">
 
-            <section data-plugin-parallax data-plugin-options="{'speed': 1.2}" class="view-pc"
+            <section data-plugin-parallax data-plugin-options="{'speed': 1.2}" class="view-pc" data-aos="fade-in"
                 style="position: relative; height: 310px; overflow: hidden;">
                 <img style="width: 100%; height: auto; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);"
                     src="{{ asset('storage/' . ($banner->content ?? '')) }}" alt="">
             </section>
 
-            <section data-plugin-parallax data-plugin-options="{'speed': 1.2}" class="view-movile"
+            <section data-plugin-parallax data-plugin-options="{'speed': 1.2}" class="view-movile" data-aos="fade-in"
                 style="position: relative; height: 80px; overflow: hidden;">
                 <img style="width: 100%; height: auto; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);"
                     src="{{ asset('storage/' . ($banner->content ?? '')) }}" alt="">
@@ -24,7 +27,7 @@
 
             <div class="container-lg pt-5">
                 <div class="row">
-                    <div class="col-md-6">
+                    <div class="col-md-6" data-aos="fade-right" data-aos-delay="200">
                         <br>
                         <h2 style="font-weight: 700;">
                             {{ $presentation[0]->content ?? '' }}
@@ -33,7 +36,7 @@
                             {{ $presentation[1]->content ?? '' }}
                         </p>
                     </div>
-                    <div class="col-md-6 ara_centrado_total">
+                    <div class="col-md-6 ara_centrado_total" data-aos="fade-left" data-aos-delay="400">
                         {!! $presentation[2]->content ?? '' !!}
                     </div>
                 </div>
@@ -47,9 +50,7 @@
                         <div class="row portfolio-list lightbox"
                             data-plugin-options="{'delegate': 'a.lightbox-portfolio', 'type': 'image', 'gallery': {'enabled': true}}">
                             @foreach ($galery as $item)
-                                <div class="col-md-4 appear-animation animated expandIn appear-animation-visible"
-                                    data-appear-animation="expandIn" data-appear-animation-delay="1000"
-                                    style="animation-delay: 100ms;">
+                                <div class="col-md-4" data-aos="fade-up" data-aos-delay="{{ ($loop->iteration * 100) }}">
                                     <div class="portfolio-item">
                                         <span
                                             class="thumb-info thumb-info-lighten thumb-info-centered-icons border-radius-0">
@@ -83,7 +84,7 @@
             <br>
             <div class="container pt-2" style="text-align:center;">
                 <div class="row">
-                    <div class="col-md-12">
+                    <div class="col-md-12" data-aos="zoom-in" data-aos-delay="200">
                         <a href="{{ $rsociales[0]->content ?? '#' }}" target="_blank">
                             <button class="boton-fuego">
                                 <i class="fab fa-facebook" style="font-size: 24px;"></i>&nbsp; Facebook
@@ -105,35 +106,49 @@
 
             <br><br>
 
-            <section
-                class="section section-parallax section-height-5 overlay overlay-show overlay-op-7 border-0 m-0 appear-animation animated fadeIn appear-animation-visible"
-                data-appear-animation="fadeIn" data-plugin-parallax=""
-                data-plugin-options="{'speed': 1.5, 'parallaxHeight': '138%'}" data-image-src="{{ asset('storage/' . ($biblico[0]->content ?? '')) }}"
-                style="position: relative; overflow: hidden; animation-delay: 100ms;">
+            {{-- INICIO: NUEVA PROPUESTA (GLASSMORPHISM) --}}
+            <style>
+                .glass-card {
+                    background: rgba(255, 255, 255, 0.1);
+                    backdrop-filter: blur(10px);
+                    -webkit-backdrop-filter: blur(10px);
+                    border: 1px solid rgba(255, 255, 255, 0.2);
+                    border-radius: 20px;
+                    padding: 3rem;
+                    box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+                }
+            </style>
+            <section class="section section-parallax section-height-5 overlay overlay-show overlay-op-7 border-0 m-0 d-flex align-items-center appear-animation"
+                data-appear-animation="fadeIn"
+                data-plugin-parallax data-plugin-options="{'speed': 1.5, 'parallaxHeight': '138%'}"
+                data-image-src="{{ asset('storage/' . ($biblico[0]->content ?? '')) }}"
+                style="min-height: 500px;">
                 <div class="container">
-                    <div class="row justify-content-center" style="padding: 80px 0px;">
-                        <div class="col-lg-9 text-center">
-                            <h2 class="text-color-light font-weight-bold custom-tertiary-font ls-0 mb-4 appear-animation animated fadeInUpShorter appear-animation-visible"
-                                data-appear-animation="fadeInUpShorter" data-appear-animation-delay="200"
-                                style="animation-delay: 200ms;">
-                                {{ $biblico[1]->content ?? '' }}
-                            </h2>
-                            <p class="text-color-light opacity-7 text-3 px-5 mx-5 mb-0 appear-animation animated fadeInUpShorter appear-animation-visible"
-                                data-appear-animation="fadeInUpShorter" data-appear-animation-delay="400"
-                                style="animation-delay: 400ms;">
-                                {{ $biblico[2]->content ?? '' }}
-                            </p>
+                    <div class="row justify-content-center">
+                        <div class="col-lg-10 col-xl-8">
+                            <div class="glass-card text-center" data-aos="zoom-in" data-aos-delay="200">
+                                <h2 class="text-white font-weight-bold text-9 mb-3" style="text-shadow: 2px 2px 4px rgba(0,0,0,0.5); font-family: 'Montserrat', serif !important;">
+                                    {{ $biblico[1]->content ?? '' }}
+                                </h2>
+                                <div class="divider divider-small divider-small-center divider-light mb-4">
+                                    <hr style="width: 50px; border-top: 3px solid #fff; opacity: 1;">
+                                </div>
+                                <p class="text-white text-4 mb-0 font-weight-light" style="line-height: 1.8; text-shadow: 1px 1px 2px rgba(0,0,0,0.5);">
+                                    {{ $biblico[2]->content ?? '' }}
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </section>
+            {{-- FIN: NUEVA PROPUESTA --}}
 
             <br><br>
 
             <div class="container-lg py-4">
                 <div class="row">
                             @foreach ($videos as $video)
-                                <div class="col-md-4" style="padding: 15px;">
+                                <div class="col-md-4" style="padding: 15px;" data-aos="fade-up" data-aos-delay="{{ ($loop->iteration * 100) }}">
                                     {!! $video->group->items[0]->content ?? '' !!}
                                 </div>
                             @endforeach
@@ -156,4 +171,13 @@
 
 
     </div>
+
+    <!-- AOS Animation JS -->
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+        AOS.init({
+            mirror: true, // Animación al hacer scroll hacia arriba
+            once: false, // Animación cada vez que se hace scroll
+        });
+    </script>
 @endsection
--- resources/views/jrrss/kids.blade.php
+++ resources/views/jrrss/kids.blade.php
@@ -4,6 +4,9 @@
 
     <div class="body">
 
+        <!-- AOS Animation CSS -->
+        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
         <!-- Header - area start -->
         <x-jrrss.header-area></x-jrrss.header-area>
         <!-- Header - area end -->
@@ -12,13 +15,13 @@
         <div role="main" class="main">
 
 
-            <section data-plugin-parallax data-plugin-options="{'speed': 1.2}" class="view-pc"
+            <section data-plugin-parallax data-plugin-options="{'speed': 1.2}" class="view-pc" data-aos="fade-in"
                 style="position: relative; height: 310px; overflow: hidden;">
                 <img style="width: 100%; height: auto; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);"
                     src="{{ asset('storage/' . ($banner->content ?? '')) }}" alt="">
             </section>
 
-            <section data-plugin-parallax data-plugin-options="{'speed': 1.2}" class="view-movile"
+            <section data-plugin-parallax data-plugin-options="{'speed': 1.2}" class="view-movile" data-aos="fade-in"
                 style="position: relative; height: 80px; overflow: hidden;">
                 <img style="width: 100%; height: auto; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);"
                     src="{{ asset('storage/' . ($banner->content ?? '')) }}" alt="">
@@ -26,7 +29,7 @@
 
             <div class="container-lg pt-5">
                 <div class="row">
-                    <div class="col-md-6">
+                    <div class="col-md-6" data-aos="fade-right" data-aos-delay="200">
                         <br>
                         <h2 style="font-weight: 700;">
                             {{ $presentacion[0]->content ?? '' }}
@@ -35,7 +38,7 @@
                             {{ $presentacion[1]->content ?? '' }}
                         </p>
                     </div>
-                    <div class="col-md-6 ara_centrado_total">
+                    <div class="col-md-6 ara_centrado_total" data-aos="fade-left" data-aos-delay="400">
                         {!! $presentacion[2]->content ?? '' !!}
                     </div>
                 </div>
@@ -49,9 +52,7 @@
                         <div class="row portfolio-list lightbox" data-plugin-options="{'delegate': 'a.lightbox-portfolio', 'type': 'image', 'gallery': {'enabled': true}}">
 
                             @foreach ($galeryKids as $item)
-                                <div class="col-md-4 appear-animation animated expandIn appear-animation-visible"
-                                    data-appear-animation="expandIn" data-appear-animation-delay="1000"
-                                    style="animation-delay: 100ms;">
+                                <div class="col-md-4" data-aos="fade-up" data-aos-delay="{{ ($loop->iteration * 100) }}">
                                     <div class="portfolio-item">
                                         <span
                                             class="thumb-info thumb-info-lighten thumb-info-centered-icons border-radius-0">
@@ -84,24 +85,41 @@
 
             </div>
 
-            <section class="section section-parallax section-height-5 overlay overlay-show overlay-op-7 border-0 m-0 appear-animation animated fadeIn appear-animation-visible"
-                    data-appear-animation="fadeIn" data-plugin-parallax="" data-plugin-options="{'speed': 1.5, 'parallaxHeight': '138%'}"
-                    data-image-src="{{ asset('storage/' . ($textBiblie[0]->content ?? '')) }}" style="position: relative; overflow: hidden; animation-delay: 100ms;">
-                    <div class="container">
-                        <div class="row justify-content-center" style="padding: 80px 0px;">
-                            <div class="col-lg-9 text-center">
-                                <h2 class="text-color-light font-weight-bold custom-tertiary-font ls-0 mb-4 appear-animation animated fadeInUpShorter appear-animation-visible"
-                                    data-appear-animation="fadeInUpShorter" data-appear-animation-delay="200" style="animation-delay: 200ms;">
+            {{-- INICIO: NUEVA PROPUESTA (GLASSMORPHISM) --}}
+            <style>
+                .glass-card {
+                    background: rgba(255, 255, 255, 0.1);
+                    backdrop-filter: blur(10px);
+                    -webkit-backdrop-filter: blur(10px);
+                    border: 1px solid rgba(255, 255, 255, 0.2);
+                    border-radius: 20px;
+                    padding: 3rem;
+                    box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+                }
+            </style>
+            <section class="section section-parallax section-height-5 overlay overlay-show overlay-op-7 border-0 m-0 d-flex align-items-center appear-animation"
+                data-appear-animation="fadeIn"
+                data-plugin-parallax data-plugin-options="{'speed': 1.5, 'parallaxHeight': '138%'}"
+                data-image-src="{{ asset('storage/' . ($textBiblie[0]->content ?? '')) }}"
+                style="min-height: 500px;">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-10 col-xl-8">
+                            <div class="glass-card text-center" data-aos="zoom-in" data-aos-delay="200">
+                                <h2 class="text-white font-weight-bold text-9 mb-3" style="text-shadow: 2px 2px 4px rgba(0,0,0,0.5); font-family: 'Montserrat', serif !important;">
                                     {{ $textBiblie[1]->content ?? '' }}
                                 </h2>
-                                <p class="text-color-light opacity-7 text-3 px-5 mx-5 mb-0 appear-animation animated fadeInUpShorter appear-animation-visible"
-                                   data-appear-animation="fadeInUpShorter" data-appear-animation-delay="400" style="animation-delay: 400ms;">
-                                   {{ $textBiblie[2]->content ?? '' }}
+                                <div class="divider divider-small divider-small-center divider-light mb-4">
+                                    <hr style="width: 50px; border-top: 3px solid #fff; opacity: 1;">
+                                </div>
+                                <p class="text-white text-4 mb-0 font-weight-light" style="line-height: 1.8; text-shadow: 1px 1px 2px rgba(0,0,0,0.5);">
+                                    {{ $textBiblie[2]->content ?? '' }}
                                 </p>
                             </div>
                         </div>
                     </div>
             </section>
+            {{-- FIN: NUEVA PROPUESTA --}}
 
             <br><br>
 
@@ -112,7 +130,7 @@
 
                         <div class="row portfolio-list lightbox" data-plugin-options="{'delegate': 'a.lightbox-portfolio', 'type': 'image', 'gallery': {'enabled': true}}">
                             @foreach ($videoteca as $video)
-                                <div class="col-md-6" style="padding: 15px;">
+                                <div class="col-md-6" style="padding: 15px;" data-aos="fade-up" data-aos-delay="{{ ($loop->iteration * 100) }}">
                                     {!! $video->group->items[0]->content ?? '' !!}
                                 </div>
                             @endforeach
@@ -142,4 +160,12 @@
 
     </div>
 
+    <!-- AOS Animation JS -->
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+        AOS.init({
+            mirror: true, // Animación al hacer scroll hacia arriba
+            once: false, // Animación cada vez que se hace scroll
+        });
+    </script>
 @endsection
